c285ee8fdab98591d7ad1751aaea4dc2bcf5c02f
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-11-25 09:44:09 +0000

    lesson_5: task_1: Done

---
+++ lesson_5/task_1/script.js
@@ -0,0 +1,12 @@
+let friends = ['Tuga', 'Vasco', 'Ricardo', 'Carvalho'];
+alert(friends[2]);
+
+let name = 'Miguel';
+if (name == 'João' || name == 'Maria')
+				alert("Esse também é o meu nome!");
+
+let myVariable = 4;
+if (myVariable > 5)
+				alert("upper");
+else
+				alert("lower");
