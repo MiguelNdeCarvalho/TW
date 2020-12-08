c6baf1646a51b798e3a76d7a22f81118c0004956
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2020-12-08 19:25:49 +0000

    AddToWaitingList: Acess public arraylist

--- lesson_6/task_1/the-restaurant-mvc/src/main/java/org/therestaurant/tweb/AddToWaitingList.java
+++ lesson_6/task_1/the-restaurant-mvc/src/main/java/org/therestaurant/tweb/AddToWaitingList.java
@@ -6,11 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
+//import java.util.ArrayList;
+//import org.therestaurant.tweb.TheRestaurant;
+
 
 @WebServlet(name = "AddToWaitingList", urlPatterns = {"waitingList"}, loadOnStartup = 1) 
 public class AddToWaitingList extends HttpServlet {
-	ArrayList<String> waitingList = new ArrayList<String>();
+	//ArrayList<String> waitingList = new ArrayList<String>();
 
 	private static final long serialVersionUID = 1L;
 
@@ -20,10 +22,10 @@ public class AddToWaitingList extends HttpServlet {
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
 		String name = request.getParameter("name");
-		waitingList.add(name);
+		TheRestaurant.waitingList.add(name);
 		request.setAttribute("clientName", name);
-		request.setAttribute("waitingSize", waitingList.size() - 1);
-	 	request.setAttribute("waitingList", waitingList);
+		request.setAttribute("waitingSize", TheRestaurant.waitingList.size() - 1);
+	 	request.setAttribute("waitingList", TheRestaurant.waitingList);
 		request.getRequestDispatcher("addClientToWaitingListResponse.jsp").forward(request, response); 
 	}
 }
