9f1aac316f262172c5ac7d67cafe47a1ba659074
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-22 22:30:34 +0000

    lesson_9: task_2: client: Add username, password and role

--- lesson_9/task_2/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/Client.java
+++ lesson_9/task_2/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/Client.java
@@ -20,17 +20,20 @@ public class Client {
 
 	protected Client() {}
 
-	public Client(String firstName, String lastName, String mail) {
+	public Client(String firstName, String lastName, String mail, String username, String password, String role) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.mail = mail;
+		this.username = username;
+		this.password = password;
+		this.role = role;
 	}
 
 	@Override
 	public String toString() {
 		return String.format(
-				"Client[id=%d, firstName='%s', lastName='%s', mail='%s']",
-				id, firstName, lastName, mail);
+				"Client[id=%d, firstName='%s', lastName='%s', mail='%s', username='%s', password= '%s', role= '%s']",
+				id, firstName, lastName, mail, username, password, role);
 	}
 
 	public Long getId() {
@@ -47,5 +50,17 @@ public class Client {
 	
 	public String getMail() {
 		return mail;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getRole() {
+		return role;
 	}
 }
