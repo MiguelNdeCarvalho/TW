1f26bd6081946834f5abbb431586557e7f238f0b
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-22 19:26:24 +0000

    Make ID generations unique for each class

--- lesson_9/task_1/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/Client.java
+++ lesson_9/task_1/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/Client.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Client {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	private String firstName, lastName, mail;
 
