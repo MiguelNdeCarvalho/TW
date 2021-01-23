7540c918e0abcb5a2beaa831d83b6ea68bb3e5e2
MiguelNdeCarvalho <geral@miguelndecarvalho.pt>
2021-01-23 17:42:02 +0000

    lesson_9: task_2: WebSecurityConfig class use authenticator

--- lesson_9/task_2/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/WebSecurityConfig.java
+++ lesson_9/task_2/the-restaurant-jpa/src/main/java/org/therestaurant/tweb/jpa/WebSecurityConfig.java
@@ -9,15 +9,43 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+
+
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsServiceImpl();
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+	}
+	
+	@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
-				.antMatchers("/", "/index").permitAll()
+				.antMatchers("/", "/index", "/new-client.html", "/new-client").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
@@ -26,19 +54,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.and()
 			.logout()
 				.permitAll();
-	}
-
-	
-	@Bean
-	@Override
-	public UserDetailsService userDetailsService() {
-		UserDetails user =
-			 User.withDefaultPasswordEncoder()
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-        
-		return new InMemoryUserDetailsManager(user);
 	}
 }
